ce6867e86400c23da1ff6c221b739d9f6ae71840
Gilbert Elnino <gilbeltelnino@gmail.com>
2020-10-01 22:01:03 +0200

    fix-cors error in the browser

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import "@babel/polyfill";
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import swaggerUi from "swagger-ui-express";
 import routers from "./routes/router";
 import { connectDB } from "./models/db";
@@ -16,6 +17,8 @@ app.use(express.json());
 app.use("/images", express.static("images"));
 // production middleware
 prodMiddleware(app);
+// cors
+app.use(cors());
 
 app.use("/api", routers);
 // swagger
